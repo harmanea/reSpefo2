6af69797c796fe86ed35564371a69dc4d35a1469
Adam Harmanec <adam.harmanec@volny.cz>
2021-10-06 14:36:47 +0200

    update BeSS fits format to also use BSS_RQHV

--- src/main/java/cz/cuni/mff/respefo/format/formats/fits/BeSSFitsFormat.java
+++ src/main/java/cz/cuni/mff/respefo/format/formats/fits/BeSSFitsFormat.java
@@ -1,12 +1,40 @@
 package cz.cuni.mff.respefo.format.formats.fits;
 
+import cz.cuni.mff.respefo.SpefoException;
+import cz.cuni.mff.respefo.format.Spectrum;
 import nom.tam.fits.Header;
 
 public class BeSSFitsFormat extends ImportFitsFormat {
 
+    private boolean applyCorrection;
+
+    @Override
+    public Spectrum importFrom(String fileName) throws SpefoException {
+        applyCorrection = false;
+
+        Spectrum spectrum = super.importFrom(fileName);
+
+        if (applyCorrection) {
+            double rvCorr = spectrum.getRvCorrection();
+            spectrum.setRvCorrection(0);
+            spectrum.updateRvCorrection(rvCorr);
+        }
+
+        return spectrum;
+    }
+
     @Override
     protected double getRVCorrection(Header header) {
-        return -header.getDoubleValue("BSS_VHEL");
+        double BSS_VHEL = header.getDoubleValue("BSS_VHEL");
+
+        if (BSS_VHEL != 0) {
+            return -BSS_VHEL;
+        }
+
+        double BSS_RQHV = header.getDoubleValue("BSS_RQHV");
+        applyCorrection = true;
+
+        return -BSS_RQHV;
     }
 
     @Override
