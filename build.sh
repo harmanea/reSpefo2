3311de26a91debd51c7d06ab6ad147481f70c316
Adam Harmanec <adam.harmanec@volny.cz>
2022-03-16 20:42:46 +0100

    upgrade library versions

--- build.sh
+++ build.sh
@@ -2,11 +2,13 @@
 
 declare -A build
 
-build[gtk.linux.x86_64]=4.12.0
+build[gtk.linux.x86_64]=4.17.0
+build[win32.win32.x86_64]=4.17.0
+build[cocoa.macosx.x86_64]=4.17.0
+
+# We no longer use legacy 32-bit versions
 # build[gtk.linux.x86]=4.9.0
-build[win32.win32.x86_64]=4.12.0
 # build[win32.win32.x86]=4.9.0
-build[cocoa.macosx.x86_64]=4.12.0
 
 for b in "${!build[@]}"; do
     gradle -PswtPlatform="$b" -PswtRelease="${build[$b]}" fatJar
