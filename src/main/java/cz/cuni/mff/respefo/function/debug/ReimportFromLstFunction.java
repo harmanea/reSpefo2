76e9c615dcb6652cdf855c7172f97a528a4282d5
Adam Harmanec <harmanec@utia.cas.cz>
2025-03-12 21:41:34 +0100

    optionally apply rv correction difference when reimporting information from a lst file

--- src/main/java/cz/cuni/mff/respefo/function/debug/ReimportFromLstFunction.java
+++ src/main/java/cz/cuni/mff/respefo/function/debug/ReimportFromLstFunction.java
@@ -16,15 +16,15 @@ import cz.cuni.mff.respefo.util.Progress;
 
 import java.io.File;
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.function.BiConsumer;
 
 @Fun(name = "Reimport from .lst", fileFilter = SpefoFormatFileFilter.class, group = "Debug")
 public class ReimportFromLstFunction implements SingleFileFunction, MultiFileFunction {
 
     @Override
     public void execute(File file) {
-        update(lst -> {
-            if (updateFile(lst, file)) {
+        update((lst, applyRvCorrection) -> {
+            if (updateFile(lst, file, applyRvCorrection)) {
                 Message.info("Update finished successfully.");
             } else {
                 Message.warning("An error occurred while updating file.");
@@ -34,13 +34,13 @@ public class ReimportFromLstFunction implements SingleFileFunction, MultiFileFun
 
     @Override
     public void execute(List<File> files) {
-        update(lst -> Progress.withProgressTracking(
+        update((lst, applyRvCorrection) -> Progress.withProgressTracking(
                 p -> {
                     p.refresh("Reimporting files", files.size());
 
                     boolean successful = true;
                     for (File file : files) {
-                        successful &= updateFile(lst, file);
+                        successful &= updateFile(lst, file, applyRvCorrection);
                         p.step();
                     }
                     return successful;
@@ -54,7 +54,7 @@ public class ReimportFromLstFunction implements SingleFileFunction, MultiFileFun
         ));
     }
 
-    private static void update(Consumer<LstFile> updater) {
+    private static void update(BiConsumer<LstFile, Boolean> updater) {
         File[] lstFiles = Project.getRootDirectory().listFiles((dir, name) -> name.endsWith(".lst"));
         if (lstFiles == null || lstFiles.length == 0) {
             Message.warning("No .lst file found in the project directory");
@@ -63,17 +63,18 @@ public class ReimportFromLstFunction implements SingleFileFunction, MultiFileFun
 
         try {
             LstFile lst = LstFile.open(lstFiles[0]);
-            updater.accept(lst);
+            boolean applyRvCorrection = Message.question("Apply RV correction difference?");
+            updater.accept(lst, applyRvCorrection);
 
         } catch (SpefoException exception) {
             Message.error("Couldn't load .lst file", exception);
         }
     }
 
-    private static boolean updateFile(LstFile lst, File file) {
+    private static boolean updateFile(LstFile lst, File file, boolean applyRvCorrection) {
         try {
             Spectrum spectrum = Spectrum.open(file);
-            ImportFunction.updateSpectrumUsingLstFile(spectrum, lst, ((BaseOrigin) spectrum.getOrigin()).getFileName(), true);
+            ImportFunction.updateSpectrumUsingLstFile(spectrum, lst, ((BaseOrigin) spectrum.getOrigin()).getFileName(), applyRvCorrection);
             spectrum.save();
             return true;
 
